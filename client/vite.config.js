bbdf4b57bd62ce9bfd2eee652c80980a85823b35
vishalP143 <vishal.p8070@gmail.com>
2025-06-26 19:04:56 +0000

    basics created

---
+++ client/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    allowedHosts: [
+      '5173-vishalp143-moviemania-c0f2jm9lpbb.ws-us120.gitpod.io'
+    ]
+  }
+})
